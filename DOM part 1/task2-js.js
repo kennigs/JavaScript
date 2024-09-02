dbc480c95340399bee09218fa98af9207f76bef7
kennigs <SammyxdOficcial@yandex.ru>
2024-09-03 00:43:25 +0800

    Add files via upload

--- DOM part 1/task2-js.js
+++ DOM part 1/task2-js.js
@@ -1,37 +1,65 @@
 const tasks = [
     {
-        id: '1138465078061',
-        completed: false,
-        text: 'Посмотреть новый урок по JavaScript',
+    id: "1138465078061",
+    completed: false,
+    text: "Посмотреть новый урок по JavaScript"
     },
     {
-        id: '1138465078062',
-        completed: false,
-        text: 'Выполнить тест после урока',
+    id: "1138465078062",
+    completed: false,
+    text: "Выполнить тест после урока"
     },
     {
-        id: '1138465078063',
-        completed: false,
-        text: 'Выполнить ДЗ после урока',
-    },
+    id: "1138465078063",
+    completed: false,
+    text: "Выполнить ДЗ после урока"
+    }
 ]
-// data-task-id
-const data = document.querySelector(`[data-task-id='1']`)
-let dd = []
-tasks.forEach(id => dd.push(id.id))
-data.setAttribute(`data-task-id`, dd[0])
-console.log(data.getAttribute(`data-task-id`))
-// id
-const id = document.querySelector(`input`)
-id.setAttribute(`id`, dd[1])
-console.log(id.getAttribute(`id`))
-// for
-const labels = document.querySelector('label')
-const forr = labels.htmlFor = dd[2]
-console.log(forr)
-// text on span
-let vv = []
-tasks.forEach(text => vv.push(text.text))
-const span = document.querySelector('span')
-span.textContent = vv
-console.log(span.textContent)
+
+const createTaskItem = (taskId, taskText) => {
+    const taskItem = document.createElement("div")
+    taskItem.className = "task-item"
+    taskItem.dataset.taskId = taskId
+
+    const taskItemMainContainer = document.createElement("div")
+    taskItemMainContainer.className = "task-item__main-container"
+
+    const taskItemMainContent = document.createElement("div")
+    taskItemMainContent.className = "task-item__main-content"
+
+    taskItem.append(taskItemMainContainer)
+    taskItemMainContainer.append(taskItemMainContent)
+
+    const checkboxForm = document.createElement("form")
+    checkboxForm.className = "checkbox-form"
+
+    const inputCheckbox = document.createElement("input")
+    inputCheckbox.type = "checkbox"
+    inputCheckbox.className = "checkbox-form__checkbox"
+    const inputId = `task-${taskId}`
+    inputCheckbox.id = inputId
+
+    const labelCheckbox = document.createElement("label")
+    labelCheckbox.htmlFor = inputId
+
+    const taskItemText = document.createElement("span")
+    taskItemText.className = "task-item__text"
+    taskItemText.innerText = taskText
+
+    const deleteButton = document.createElement("button")
+    deleteButton.className = "task-item__delete-button default-button delete-button"
+    deleteButton.innerText = "Удалить"
+
+    taskItemMainContent.append(checkboxForm, taskItemText)
+    checkboxForm.append(inputCheckbox, labelCheckbox)
+    taskItemMainContainer.append(deleteButton)
+
+    return taskItem
+}
+
+const tasksListContainer = document.querySelector(".tasks-list")
+tasks.forEach((task) => {
+    const taskItem = createTaskItem(task.id, task.text)
+    tasksListContainer.append(taskItem)
+})
+
